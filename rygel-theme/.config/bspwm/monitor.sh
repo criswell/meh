e840c0b0b68bbd9193620d34221d5dd70b3dfe12
Sam Hart <hartsn@gmail.com>
2020-05-09 15:35:48 -0400

    move stuff to legacy, new rygel theme

---
+++ rygel-theme/.config/bspwm/monitor.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# Logic for the monitor setup
+
+bspc monitor -d I II III IV V VI VII VIII IX X
---
+++ rygel-theme/.config/bspwm/window_setup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Logic for the window setup
+
+bspc config border_width         1
+bspc config window_gap           3
+
+bspc config split_ratio          0.52
+bspc config borderless_monocle   true
+bspc config gapless_monocle      true
+bspc config top_padding 25
