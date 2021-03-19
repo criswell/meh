0745d064c2219d22d98f8a6502846698938156ff
Sam Hart <hartsn@gmail.com>
2021-03-19 15:10:21 -0400

    comment because later I may want to clean this kruft

--- bijaz-theme/.config/bspwm/plasma_settings.sh
+++ bijaz-theme/.config/bspwm/plasma_settings.sh
@@ -7,6 +7,7 @@
 
 bspc rule -a krunner state=floating sticky=off border=off layer=below
 bspc rule -a lattedock sticky=off border=off state=floating
+# THIS IS THE ONE THAT WORKS... I'm not sure the others are needed
 bspc rule -a plasmashell state=floating sticky=off border=off center=off
 bspc rule -a plasma state=floating border=off layer=below center=off
 bspc rule -a plasma-desktop state=floating border=off layer=below center=off
