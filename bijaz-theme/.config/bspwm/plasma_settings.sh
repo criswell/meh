755508bb5f0dc3c9d6c7995218ba876d552fdf52
Sam Hart <hartsn@gmail.com>
2021-03-19 15:05:16 -0400

    trying to get that pesky plasma to play nice again

---
+++ bijaz-theme/.config/bspwm/plasma_settings.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+# Various KDE things, currently ripped from
+#   https://www.reddit.com/r/bspwm/comments/j80wqg/plasma_notifications_menu_appearing_as_a_window/
+# Probably will want to re-evaluate all this since I'm not 100% what it all
+# does.
+
+bspc rule -a krunner state=floating sticky=off border=off layer=below
+bspc rule -a lattedock sticky=off border=off state=floating
+bspc rule -a plasmashell state=floating sticky=off border=off center=off
+bspc rule -a plasma state=floating border=off layer=below center=off
+bspc rule -a plasma-desktop state=floating border=off layer=below center=off
